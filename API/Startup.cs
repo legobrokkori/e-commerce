bb2c62a60d7bb671efec173c4704f1f462b70b78
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-07 15:00:35 +0900

    Add origin policy for angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,13 @@ namespace API
             
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +51,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UserSwaggerDocumentation();
             app.UseEndpoints(endpoints =>
