da14203bd7c81862ee2d2aff7f2cc7abfe6b09c4
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-13 17:34:46 +0900

    Design top page & Clean up folder structure

---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from '../shared/models/product';
+import { ShopService } from './shop.service';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  products: IProduct[];
+
+  constructor(private shopService: ShopService) { }
+
+  ngOnInit(): void {
+    this.shopService.getProducts().subscribe(res => {
+      this.products = res.data;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+}
