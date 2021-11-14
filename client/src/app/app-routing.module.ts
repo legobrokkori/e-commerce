6d231cea6223e69715e4757894b3dc3305342eb1
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-14 14:01:32 +0900

    deleted unnecessary import

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
