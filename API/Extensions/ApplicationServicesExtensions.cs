681cf04fef95dcd719355042aa8b5c5e8857505e
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-21 15:41:17 +0900

    Add backet function with redis

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -12,6 +12,7 @@ namespace API.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped(typeof(IProductRepository<>), (typeof(ProductRepository<>)));
+            services.AddScoped<IBasketRepository, BasketReposirory>();
              
             services.Configure<ApiBehaviorOptions>(options =>
             {
