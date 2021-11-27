1be00d5627e72bacfeed70159f20f4645beb6ebf
Kazuhiro Watanabe <ralirulego103@gmail.com>
2021-11-27 18:36:57 +0900

    added login and regiter

---
+++ API/ Dtos/UserDto.cs
@@ -0,0 +1,8 @@
+namespace API._Dtos
+{
+    public class UserDto{
+        public string Email { get; set; }
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+    }
+}
